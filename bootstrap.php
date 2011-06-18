ca4493142f1de0a89e4e2c2c689e9ca0f1e40ade
lgadban <lgadban@942dca17-fbde-0292-d59d-a099d8e55517>
2011-06-18 00:44:03 +0000

    Fixed dependencies for database values

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,7 @@
 <?php
 	if(!isset($_SESSION['SESS_MEMBER_ID']))			// start session if it hasn't been
 		session_start();
-		
-	require_once('/electronic-mis/config.php');		// database connection details
-	
+			
 	// connect to mysql server
 	$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 	if(!$link) {
