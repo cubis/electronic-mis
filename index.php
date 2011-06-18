8b7228b9ac3fdfd04eebb9e2df25ac22cb6d638f
lgadban <lgadban@942dca17-fbde-0292-d59d-a099d8e55517>
2011-06-18 00:08:17 +0000

    Changed index.php to be the login form

--- index.php
+++ index.php
@@ -1,6 +1,42 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Login Form</title>
+<link href="loginmodule.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<h1 align="center">Welcome, EMIS Login</h1>
+<p>&nbsp;</p>
+<form id="loginForm" name="loginForm" method="post" action="login-exec.php">
 
-<html>
-	<h1> Welcome to the B team</h1>
-	<a href="login-form.php">Log-in</a>
+  <table width="210" border="0" align="center" cellpadding="2" cellspacing="3">
+    <tr>
+
+      <td width="112"><b>Login:</b></td>
+      <td width="188"><input name="login" type="text" class="textfield" id="login" /></td>
+    </tr>
+    <tr>
+      <td><b>Password:</b></td>
+      <td><input name="password" type="password" class="textfield" id="password" /></td>
+    </tr>
+    <tr>
+
+      <td></td>
+      <td>
+          <input type="submit" name="Submit" value="Login" />
+      </td>
+    </tr>
+      <tr>
+          <td></td><td>
+          <a href="register-form.php">Register</a>
+
+      </td>
+    </tr>
+  </table>
+  
+</form>
+</body>
+
 </html>
+
