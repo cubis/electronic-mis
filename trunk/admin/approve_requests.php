b1e0dc5f8b6ebb4071d3686608a8816b9f53cb51
lgadban <lgadban@942dca17-fbde-0292-d59d-a099d8e55517>
2011-06-21 01:46:17 +0000

    Admin approval of requests works now. Better functionality still needed.

--- trunk/admin/approve_requests.php
+++ trunk/admin/approve_requests.php
@@ -6,7 +6,34 @@
 		$qry="SELECT * FROM Users WHERE NeedApproval='1'";
 		$result=mysql_query($qry);
 ?>
+<?php
+if(isset($_POST["submitted"])):
 
+foreach ($_POST as $username => $value)
+{
+	if(strcmp($value,"approve") == 0)
+	{
+		$qry = "UPDATE Users SET NeedApproval='0' WHERE UserName='$username'";
+		$result=mysql_query($qry);
+		if ($result)
+			echo "Approved user '$username' <br />\n";
+		else
+			echo "Error: ",mysql_error($link),"<br />\n";
+	}
+	elseif(strcmp($value,"deny") == 0)
+	{
+		echo " Denying user '$key' <br />\n";
+	}
+	else
+	{
+		echo "<br />";
+	}
+}
+
+?>
+
+
+<?php else: ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -19,17 +46,22 @@
 <hr />
 <br />
 <a href="../member-profile.php">Return to Profile</a>
-
+<br />
+<br />
 <table border="1">
 <tr>
 <td>First Name</td><td>Last Name</td><td>Sex</td><td>Username</td>
 <td>Email</td><td>Birthday</td><td>Phone Number</td><td>SSN</td><td>Type</td><td>Approval</td>
 </tr>
+
+<form id="approvalForm" name="approvalForm" method="post" action="approve_requests.php">
+<input type="hidden" name="submitted" value="true" />
 <?php
 $numrows = mysql_num_rows($result);
 
 while ($row = mysql_fetch_assoc($result))
 {
+	$username = $row['UserName'];
 	echo "<tr>\n";
 	echo "<td>",$row['FirstName'],"</td>\n";
 	echo "<td>",$row['LastName'],"</td>\n";
@@ -40,13 +72,19 @@ while ($row = mysql_fetch_assoc($result))
 	echo "<td>",$row['PhoneNumber'],"</td>\n";
 	echo "<td>",$row['SSN'],"</td>\n";
 	echo "<td>",$row['Type'],"</td>\n";
-	echo "<td><a href='#'>Edit</a></td>\n";
+	echo "<td><input type='radio' name='$username' value='approve' /> Approve <input type='radio' name='$username' value='deny' /> Deny</td>\n";
 	echo "</tr>\n";
 }
 ?>
 
 
 </table>
+<br />
+<br />
+<input type="submit" />
+
 
 </body>
 </html>
+<?php endif;
+?>
