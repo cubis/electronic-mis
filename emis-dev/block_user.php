ba3ab2acd874a525f6a219f9745fb683d422a4e2
jbutron31 <jbutron31@gmail.com>
2011-07-01 20:59:26 +0000

    error page

---
+++ emis-dev/block_user.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>Your account has been blocked</title>
+	</head>
+	<body>
+	<center><h1 style="color: black; margin-top: 50px;">Account blocked by admin</h1>
+	</center>
+	
+	</body>
