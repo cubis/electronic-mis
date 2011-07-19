9db6980bf907be697320633e5436d54f780435d6
danielcurran90@gmail.com <danielcurran90@gmail.com@942dca17-fbde-0292-d59d-a099d8e55517>
2011-07-19 22:37:34 +0000

    I changed a bunch of stuff. PDO is now used for all queries...it makes them parameterized Login won't work on locked accounts REST does all checking for login (make sure password and username are there) auth key is now created based on time and is saved to the database as the "currentKey" logout doesn't work right now but it should remove this key on logout

---
+++ trunk/emis-iter3/auth.php
@@ -0,0 +1,14 @@
+<?php
+
+//Start session
+session_start();
+
+/**************JUST MAKES SURE THE CLIENT SIDE DOESN'T SHOW SITES YOU CAN'T GET INFO FOR***********/
+
+
+//Check whether the session variable SESS_MEMBER_ID is present or not
+if (!isset($_SESSION['SESS_MEMBER_ID']) || (trim($_SESSION['SESS_MEMBER_ID']) == '')) {
+    header("location: access-denied.php");
+    exit();
+}
+?>
