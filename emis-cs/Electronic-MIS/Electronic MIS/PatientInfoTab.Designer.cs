dae33a3136741dac8a12455a8fff72981d452d90
and1balla2424 <AND1Balla2424@gmail.com>
2011-07-22 02:20:22 +0000

    Moved code from Login.cs into LoginTab.cs and did the PatientInfoTab.cs design.

--- emis-cs/Electronic-MIS/Electronic MIS/PatientInfoTab.Designer.cs
+++ emis-cs/Electronic-MIS/Electronic MIS/PatientInfoTab.Designer.cs
@@ -28,19 +28,416 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.firstname = new System.Windows.Forms.Label();
+            this.lastname = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.sex = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.address = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.Birthday = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SSN = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.Email = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.phonenumber = new System.Windows.Forms.Label();
+            this.userinfo = new System.Windows.Forms.Label();
+            this.contactinfo = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.Insuranceinfo = new System.Windows.Forms.Label();
+            this.CompanyName = new System.Windows.Forms.Label();
+            this.Plantype = new System.Windows.Forms.Label();
+            this.PlanNum = new System.Windows.Forms.Label();
+            this.CoveragePerent = new System.Windows.Forms.Label();
+            this.CoPay = new System.Windows.Forms.Label();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.CoverageStart = new System.Windows.Forms.Label();
+            this.CoverageEnd = new System.Windows.Forms.Label();
+            this.PreExCond = new System.Windows.Forms.Label();
+            this.PreExCondBox = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // firstname
+            // 
+            this.firstname.AutoSize = true;
+            this.firstname.Location = new System.Drawing.Point(23, 51);
+            this.firstname.Name = "firstname";
+            this.firstname.Size = new System.Drawing.Size(60, 13);
+            this.firstname.TabIndex = 1;
+            this.firstname.Text = "First Name:";
+            // 
+            // lastname
+            // 
+            this.lastname.AutoSize = true;
+            this.lastname.Location = new System.Drawing.Point(22, 77);
+            this.lastname.Name = "lastname";
+            this.lastname.Size = new System.Drawing.Size(61, 13);
+            this.lastname.TabIndex = 2;
+            this.lastname.Text = "Last Name:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(89, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // sex
+            // 
+            this.sex.AutoSize = true;
+            this.sex.Location = new System.Drawing.Point(55, 103);
+            this.sex.Name = "sex";
+            this.sex.Size = new System.Drawing.Size(28, 13);
+            this.sex.TabIndex = 4;
+            this.sex.Text = "Sex:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(89, 96);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(89, 122);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // address
+            // 
+            this.address.AutoSize = true;
+            this.address.Location = new System.Drawing.Point(35, 129);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(48, 13);
+            this.address.TabIndex = 7;
+            this.address.Text = "Address:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(89, 148);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 8;
+            // 
+            // Birthday
+            // 
+            this.Birthday.AutoSize = true;
+            this.Birthday.Location = new System.Drawing.Point(35, 155);
+            this.Birthday.Name = "Birthday";
+            this.Birthday.Size = new System.Drawing.Size(48, 13);
+            this.Birthday.TabIndex = 9;
+            this.Birthday.Text = "Birthday:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(89, 174);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 10;
+            // 
+            // SSN
+            // 
+            this.SSN.AutoSize = true;
+            this.SSN.Location = new System.Drawing.Point(51, 181);
+            this.SSN.Name = "SSN";
+            this.SSN.Size = new System.Drawing.Size(32, 13);
+            this.SSN.TabIndex = 11;
+            this.SSN.Text = "SSN:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(89, 272);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 12;
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.Location = new System.Drawing.Point(48, 279);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(35, 13);
+            this.Email.TabIndex = 13;
+            this.Email.Text = "Email:";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(89, 298);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 14;
+            // 
+            // phonenumber
+            // 
+            this.phonenumber.AutoSize = true;
+            this.phonenumber.Location = new System.Drawing.Point(2, 305);
+            this.phonenumber.Name = "phonenumber";
+            this.phonenumber.Size = new System.Drawing.Size(81, 13);
+            this.phonenumber.TabIndex = 15;
+            this.phonenumber.Text = "Phone Number:";
+            // 
+            // userinfo
+            // 
+            this.userinfo.AutoSize = true;
+            this.userinfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userinfo.Location = new System.Drawing.Point(22, 12);
+            this.userinfo.Name = "userinfo";
+            this.userinfo.Size = new System.Drawing.Size(176, 20);
+            this.userinfo.TabIndex = 16;
+            this.userinfo.Text = "Personal Information";
+            this.userinfo.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // contactinfo
+            // 
+            this.contactinfo.AutoSize = true;
+            this.contactinfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contactinfo.Location = new System.Drawing.Point(22, 237);
+            this.contactinfo.Name = "contactinfo";
+            this.contactinfo.Size = new System.Drawing.Size(169, 20);
+            this.contactinfo.TabIndex = 17;
+            this.contactinfo.Text = "Contact Information";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(408, 44);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 18;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(408, 70);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 19;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(408, 96);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(100, 20);
+            this.textBox11.TabIndex = 20;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(408, 122);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(100, 20);
+            this.textBox12.TabIndex = 21;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(408, 148);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(100, 20);
+            this.textBox13.TabIndex = 22;
+            // 
+            // Insuranceinfo
+            // 
+            this.Insuranceinfo.AutoSize = true;
+            this.Insuranceinfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Insuranceinfo.Location = new System.Drawing.Point(302, 12);
+            this.Insuranceinfo.Name = "Insuranceinfo";
+            this.Insuranceinfo.Size = new System.Drawing.Size(186, 20);
+            this.Insuranceinfo.TabIndex = 23;
+            this.Insuranceinfo.Text = "Insurance Information";
+            // 
+            // CompanyName
+            // 
+            this.CompanyName.AutoSize = true;
+            this.CompanyName.Location = new System.Drawing.Point(317, 51);
+            this.CompanyName.Name = "CompanyName";
+            this.CompanyName.Size = new System.Drawing.Size(85, 13);
+            this.CompanyName.TabIndex = 24;
+            this.CompanyName.Text = "Company Name:";
+            this.CompanyName.Click += new System.EventHandler(this.Insuranceid_Click);
+            // 
+            // Plantype
+            // 
+            this.Plantype.AutoSize = true;
+            this.Plantype.Location = new System.Drawing.Point(344, 77);
+            this.Plantype.Name = "Plantype";
+            this.Plantype.Size = new System.Drawing.Size(58, 13);
+            this.Plantype.TabIndex = 25;
+            this.Plantype.Text = "Plan Type:";
+            // 
+            // PlanNum
+            // 
+            this.PlanNum.AutoSize = true;
+            this.PlanNum.Location = new System.Drawing.Point(331, 103);
+            this.PlanNum.Name = "PlanNum";
+            this.PlanNum.Size = new System.Drawing.Size(71, 13);
+            this.PlanNum.TabIndex = 26;
+            this.PlanNum.Text = "Plan Number:";
+            // 
+            // CoveragePerent
+            // 
+            this.CoveragePerent.AutoSize = true;
+            this.CoveragePerent.Location = new System.Drawing.Point(306, 129);
+            this.CoveragePerent.Name = "CoveragePerent";
+            this.CoveragePerent.Size = new System.Drawing.Size(96, 13);
+            this.CoveragePerent.TabIndex = 27;
+            this.CoveragePerent.Text = "Coverage Percent:";
+            // 
+            // CoPay
+            // 
+            this.CoPay.AutoSize = true;
+            this.CoPay.Location = new System.Drawing.Point(358, 155);
+            this.CoPay.Name = "CoPay";
+            this.CoPay.Size = new System.Drawing.Size(44, 13);
+            this.CoPay.TabIndex = 28;
+            this.CoPay.Text = "Co-Pay:";
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(408, 175);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(100, 20);
+            this.textBox14.TabIndex = 29;
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(408, 202);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(100, 20);
+            this.textBox15.TabIndex = 30;
+            // 
+            // CoverageStart
+            // 
+            this.CoverageStart.AutoSize = true;
+            this.CoverageStart.Location = new System.Drawing.Point(321, 178);
+            this.CoverageStart.Name = "CoverageStart";
+            this.CoverageStart.Size = new System.Drawing.Size(81, 13);
+            this.CoverageStart.TabIndex = 31;
+            this.CoverageStart.Text = "Coverage Start:";
+            // 
+            // CoverageEnd
+            // 
+            this.CoverageEnd.AutoSize = true;
+            this.CoverageEnd.Location = new System.Drawing.Point(324, 205);
+            this.CoverageEnd.Name = "CoverageEnd";
+            this.CoverageEnd.Size = new System.Drawing.Size(78, 13);
+            this.CoverageEnd.TabIndex = 32;
+            this.CoverageEnd.Text = "Coverage End:";
+            // 
+            // PreExCond
+            // 
+            this.PreExCond.AutoSize = true;
+            this.PreExCond.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PreExCond.Location = new System.Drawing.Point(306, 243);
+            this.PreExCond.Name = "PreExCond";
+            this.PreExCond.Size = new System.Drawing.Size(194, 20);
+            this.PreExCond.TabIndex = 34;
+            this.PreExCond.Text = "Pre Existing Conditions";
+            // 
+            // PreExCondBox
+            // 
+            this.PreExCondBox.FormattingEnabled = true;
+            this.PreExCondBox.Location = new System.Drawing.Point(309, 279);
+            this.PreExCondBox.Name = "PreExCondBox";
+            this.PreExCondBox.Size = new System.Drawing.Size(190, 134);
+            this.PreExCondBox.TabIndex = 35;
             // 
             // PatientInfoTab
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.PreExCondBox);
+            this.Controls.Add(this.PreExCond);
+            this.Controls.Add(this.CoverageEnd);
+            this.Controls.Add(this.CoverageStart);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.CoPay);
+            this.Controls.Add(this.CoveragePerent);
+            this.Controls.Add(this.PlanNum);
+            this.Controls.Add(this.Plantype);
+            this.Controls.Add(this.CompanyName);
+            this.Controls.Add(this.Insuranceinfo);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.contactinfo);
+            this.Controls.Add(this.userinfo);
+            this.Controls.Add(this.phonenumber);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.SSN);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.Birthday);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.sex);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lastname);
+            this.Controls.Add(this.firstname);
+            this.Controls.Add(this.textBox1);
             this.Name = "PatientInfoTab";
-            this.Size = new System.Drawing.Size(378, 319);
+            this.Size = new System.Drawing.Size(730, 429);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label firstname;
+        private System.Windows.Forms.Label lastname;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label sex;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label address;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label Birthday;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label SSN;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label phonenumber;
+        private System.Windows.Forms.Label userinfo;
+        private System.Windows.Forms.Label contactinfo;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.Label Insuranceinfo;
+        private System.Windows.Forms.Label CompanyName;
+        private System.Windows.Forms.Label Plantype;
+        private System.Windows.Forms.Label PlanNum;
+        private System.Windows.Forms.Label CoveragePerent;
+        private System.Windows.Forms.Label CoPay;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.Label CoverageStart;
+        private System.Windows.Forms.Label CoverageEnd;
+        private System.Windows.Forms.Label PreExCond;
+        private System.Windows.Forms.ListBox PreExCondBox;
 
     }
 }
