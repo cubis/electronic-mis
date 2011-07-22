8c50afe05a8b45d3ba0f2af62d65b7de8478b51c
thenewzerov <thenewzerov@gmail.com>
2011-07-22 00:36:27 +0000

    Started new layout for the C# client.  Converting to tabbed layout.

--- emis-cs/Electronic-MIS/Electronic MIS/Appointments.cs
+++ emis-cs/Electronic-MIS/Electronic MIS/Appointments.cs
@@ -6,23 +6,42 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Net;
+using System.Xml;
+using System.IO;
+
+
+/*
+ * <appt>
+ *  <appID>1234567</appID>
+ *  <date>7/11/2012</date>
+ *  <time>09:45</time>
+ *  <doctor>Gonzalez</doctor>
+ *  <reason>I gots the herps</reason>
+ *  <remind>true</remind>
+ * </appt>
+ * 
+ * */
+
+
 
 namespace Electronic_MIS
 {
     public partial class Appointments : Form
     {
         SessionManager sessionManager;
+        List<Appt> appointments;
 
         public Appointments(SessionManager session)
         {
             InitializeComponent();
             sessionManager = session;
+            appointments = new List<Appt>();
         }
 
         private void Appointments_Load(object sender, EventArgs e)
         {
-         //TODO
-            //Add in the code here to read in appointments and fill in the calendar
+
 
             listBox1.Items.Add(DateTime.Today.ToLongDateString() + " " + DateTime.Today.ToLongTimeString());            
         }
@@ -61,6 +80,144 @@ namespace Electronic_MIS
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void getAppointments()
+        {
+            //Build the Connection String
+            UriBuilder ub = new UriBuilder();
+
+            StringBuilder data = new StringBuilder();
+            data.Append("u=" + WebUtility.HtmlEncode(sessionManager.User));
+            data.Append("&key=" + WebUtility.HtmlEncode(sessionManager.Key));
+
+            ub.Host = "67.10.181.224/~cookie/emis-dev/Appointments.php";
+            ub.Query = data.ToString();
+
+            //Create the request
+            Uri requestUri = ub.Uri;
+            WebRequest request = WebRequest.Create(requestUri);
+            request.Method = "GET";
+
+            try
+            {
+                WebResponse response = request.GetResponse();
+
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                XmlTextReader xmlReader = new XmlTextReader(response.GetResponseStream());
+                while (xmlReader.Read())
+                {
+                    switch (xmlReader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            if (xmlReader.Name == "appt")
+                            {
+                                Appt newAppt = new Appt();
+                                xmlReader.Read();
+                                switch (xmlReader.Name)
+                                {
+                                    case "date":
+                                        newAppt.AppointmentTime = xmlReader.ReadElementContentAsDateTime();
+                                        break;
+                                    case "time":
+                                        DateTime time = xmlReader.ReadElementContentAsDateTime();
+                                        newAppt.AppointmentTime.AddHours(time.Hour);
+                                        newAppt.AppointmentTime.AddMinutes(time.Minute);
+                                        break;
+
+                                    default:
+                                        break;
+                                }
+
+                                appointments.Add(newAppt);
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message, "Yeah...we didn't plan for this", MessageBoxButtons.OK);
+                Application.Exit();
+            }             
+        }
+    }
+
+    class Appt
+    {
+        String doc;
+        String reason;
+        int apptID;
+        bool remind;
+        DateTime appTime;
+
+
+        public String Doctor
+        {
+            get
+            {
+                return doc;
+            }
+            set
+            {
+                doc = value;
+            }
+
+        }
+
+        public String Reason
+        {
+            get
+            {
+                return reason;
+            }
+            set
+            {
+                reason = value;
+            }
+
+        }
+
+        public int AppointmentID
+        {
+            get
+            {
+                return apptID;
+            }
+            set
+            {
+                apptID = value;
+            }
+
+        }
+
+        public bool Remind
+        {
+            get
+            {
+                return remind;
+            }
+            set
+            {
+                remind = value;
+            }
+
+        }
+
+        public DateTime AppointmentTime
+        {
+            get
+            {
+                return appTime;
+            }
+            set
+            {
+                appTime = value;
+            }
+
         }
     }
 }
--- emis-cs/Electronic-MIS/Electronic MIS/MainForm.Designer.cs
+++ emis-cs/Electronic-MIS/Electronic MIS/MainForm.Designer.cs
@@ -28,10 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Login");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("View Your Patient Information");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("View Your Appointments");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Home", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3});
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.navigationTree = new System.Windows.Forms.TreeView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.TabViewer = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -40,30 +59,110 @@
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(675, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // fileToolStripMenuItem
             // 
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loginToolStripMenuItem,
+            this.logoutToolStripMenuItem,
             this.exitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
+            // loginToolStripMenuItem
+            // 
+            this.loginToolStripMenuItem.Name = "loginToolStripMenuItem";
+            this.loginToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.loginToolStripMenuItem.Text = "Login";
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.logoutToolStripMenuItem.Text = "Logout";
+            // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.navigationTree);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.tabControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(675, 321);
+            this.splitContainer1.SplitterDistance = 199;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // navigationTree
+            // 
+            this.navigationTree.Location = new System.Drawing.Point(0, 3);
+            this.navigationTree.Name = "navigationTree";
+            treeNode1.Name = "LoginNode";
+            treeNode1.Text = "Login";
+            treeNode2.Name = "InfoNode";
+            treeNode2.Text = "View Your Patient Information";
+            treeNode3.Name = "AppointmentNode";
+            treeNode3.Text = "View Your Appointments";
+            treeNode4.Name = "HomeNode";
+            treeNode4.Text = "Home";
+            this.navigationTree.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode4});
+            this.navigationTree.Size = new System.Drawing.Size(199, 318);
+            this.navigationTree.TabIndex = 0;
+            this.navigationTree.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.navigationTree_AfterSelect);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.TabViewer);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(4, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(468, 318);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // TabViewer
+            // 
+            this.TabViewer.Location = new System.Drawing.Point(4, 22);
+            this.TabViewer.Name = "TabViewer";
+            this.TabViewer.Padding = new System.Windows.Forms.Padding(3);
+            this.TabViewer.Size = new System.Drawing.Size(460, 292);
+            this.TabViewer.TabIndex = 0;
+            this.TabViewer.Text = "tabPage1";
+            this.TabViewer.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(460, 292);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(675, 345);
+            this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
@@ -71,6 +170,11 @@
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,5 +185,12 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView navigationTree;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage TabViewer;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
