8fed50aba543e23208291cf74dbd580fd58a23fd
scott.amble <scott.amble@942dca17-fbde-0292-d59d-a099d8e55517>
2011-07-23 00:20:52 +0000

    new file

---
+++ emis-dev3/visitExec.php
@@ -0,0 +1,106 @@
+<?php
+
+        //Start session
+        //session_start();
+
+
+        //Array to store validation errors
+        $errmsg_arr = array();
+
+        //Validation error flag
+        $errflag = false;
+
+        //Function to sanitize values received from the form. Prevents SQL injection
+        function clean($str) {
+                $str = @trim($str);
+                if (get_magic_quotes_gpc ()) {
+                        $str = stripslashes($str);
+                }
+                return mysql_real_escape_string($str);
+        }
+
+        //Sanitize the POST values
+		$id = $_POST['id'];
+        $bp = $_POST['bp'];
+        $weight = $_POST['weight'];
+        $sym = $_POST['sym'];
+        $diag = $_POST['diag'];
+        $med = $_POST['med'];
+        $dos = $_POST['dos'];
+        $sdate = $_POST['sdate'];
+        $edate = $_POST['edate'];
+        $pp = $_POST['pp'];
+		$numon = $_POST['nummonths'];
+		$rd = $_POST['rd'];
+		$fname = $_POST['fname'];
+		$floc = $_POST['floc'];
+							
+
+        $url = "http://localhost/emis/emis-dev3/visitREST.php";
+
+        $fields = array(
+			'bp' => urlencode($fname), 
+			'weight' => urlencode($lname),
+			'sym' => urlencode($bday), 
+			'diag' => urlencode($email),
+			'med' => urlencode($ssn), 
+			'dos' => urlencode($login), 
+			'sdate' => urlencode($password),
+			'edate' => urlencode($cpassword),
+			'pp' => urlencode($type),
+			'numon' => urlencode($numon),
+			'rd' => urlencode($rd),
+			'fname' => urlencode($fname),
+			'floc' => urlencode($floc)
+			
+        );
+
+        foreach($fields as $key=>$value){
+                $field_string .= $key.'='.$value.'&';
+        }
+  
+        rtrim($field_string, '&');
+        
+        
+            
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_POST, count($fields) );
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $field_string);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 4);    
+        curl_setopt($ch, CURLOPT_TIMEOUT, 8);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($ch);
+    
+        curl_close($ch);
+   
+        $parser = xml_parser_create();
+        
+        xml_parse_into_struct($parser, $output, $wsResponse, $wsIndices);
+        
+        
+
+        $errNum = $wsResponse[$wsIndices['ERRNUM'][0]]['value'];
+        
+        if($errNum > 0){
+                $errflag = true;
+                $ct = 0;
+                while($ct < $errNum){
+                        $errmsg_arr[] = $wsResponse[$wsIndices['ERROR'][$ct]]['value'];
+                        $ct += 1;
+                }
+        }
+        
+        //If there are input validations, redirect back to the registration form
+        if ($errflag) {
+                $_SESSION['ERRMSG_ARR'] = $errmsg_arr;
+                session_write_close();
+                header("location: visit.php");
+                exit();
+        } else {
+                header("location: visitSuccess.php");
+                exit();
+        }
+
+        
+?>
