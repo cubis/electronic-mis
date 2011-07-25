90be7b50e1692b1ab5b55110d3f5de00e3dd4c9b
basilsattler <basilsattler@gmail.com>
2011-07-25 08:03:36 +0000

    notify works, will be half restful - no outputs

--- TestFolder/notify.php
+++ TestFolder/notify.php
@@ -1,19 +1,33 @@
 <?php
-	echo "<p>start</p>";
-	if(isset($_GET['u']) and $_GET['u'] =='basil'){
-	    echo "<p>user is: ".$_GET['u']."</p>";
+/*
+plan:
+    - authenticate requestor
+    - determin requestType - {notify, logout}
+    - notify
+        - get list of users where appointment < 24 hours and wants notify=true
+            -required info {apointmentDate, Time, Doctor}
+        - send notification e-mail, on success set notify=false
+            -succesful notifaction makes action log entry
+*/
+    function notifyUser($result){
+        
+    }
+
+
+    if(isset($_GET['u']) and $_GET['u'] =='basil'){
+        echo "<p>user is: ".$_GET['u']."</p>";
         $to = "basilsattler@gmail.com";
         $subject = "Test mail from emis";
-        $message = "Hello! This is a simple email message.\n no action required!";
-        $from = "basilsattler@gmail.com";
+        $message = "this should work, Hello! This is a simple email message.\n no action required!";
+        $from = "cpe-67-10-181-224.satx.res.rr.com";
         $headers = "From:" . $from;
-        
-        mail($to,$subject,$message,$headers);
-        
-        echo "<p>Mail Sent.</p>";
-	}
-	else
-	    echo "<p>u= was not set correctly</p>\n";
-	echo "<p>done...</p>";
-	
+     
+        if( mail($to,$subject,$message,$headers)){
+            echo "<p>Mail Sent. to $to</p>";
+        }else
+            echo "<p>error occured in mail() function for $to</p>";
+        }
+    else
+        echo "<p>u= was not set correctly</p>\n";
+    echo "<p>done...</p>";
 ?>
