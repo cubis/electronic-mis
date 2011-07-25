e6863cadda93273c7e665583d0ae8e717882cee3
scott.amble <scott.amble@942dca17-fbde-0292-d59d-a099d8e55517>
2011-07-25 00:50:01 +0000

    Major rievision, finalized, should work for 3 scenarios(new appt, reschedule/cancel appt, and visit) might need three more scenarios for untrusted

--- emis-dev3/visitREST.php
+++ emis-dev3/visitREST.php
@@ -24,10 +24,17 @@ require_once('bootstrapREST.php');  //link information
 
 //Test to see if this is either an new or old appoint
 //Wherther there exists an appt id
-if($_POST['id'])
-	$output = doServiceVi($db);
-else
+//type1 =  insert (create)
+//type 2 = update (execute)
+//type 3 = reschedule;
+if($_POST['type'] == 1)
 	$output = doServiceAp($db);
+else if($_POST['type'] == 2)
+	$output = doServiceVi($db);
+else if($_POST['type'] == 3)
+	$output = doServiceUp($db);
+else
+	$output = "ERROR NO TYPE SPECIFIED IN EXECUTION CODE";
 	
 print($output);
 
@@ -58,9 +65,73 @@ function outputXML($errNum, $errMsgArr, $db) {
         $outputString .= "</content>";  
         return $outputString;   
 }
+//Used when visit happend
 
+//Type 1 service
+function doServiceAp($db) {
+		//NOTE: GET WITH MU TO SEE WHAT HE USING TO POST
+        $errMsgArr = array();
+        $errNum = 0;
+		$doc = $_POST[''];
+		$pat = $_POST[''];
+        $date = $_POST[''];
+        $time = $_POST[''];
+        $address = $_POST[''];
+        $status = $_POST[''];
+		$reason = $_POST[''];
+        /*
+        //Input Validations (still need to do
+        if (!isset($_POST['bp']) || $_POST['bp'] == '') {
+                $errMsgArr[] = 'Blood Pressure missing';
+                $errNum += 1;
+        }
+        if (!isset($_POST['weight']) || $_POST['weight'] == '') {
+                $errMsgArr[] = 'Weight missing';
+                $errNum += 1;
+        }
+        //test
+        if (!isset($_POST['sym']) || $_POST['sym'] == '') {
+                $errMsgArr[] = 'Symptoms missing';
+                $errNum += 1;
+        }
+        if (!isset($_POST['diag']) || $_POST['diag'] == '') {
+                $errMsgArr[] = 'Diagnosis address missing';
+                $errNum += 1;
+        }*/
+        
+        if($errNum == 0){
+			//set up and insert values into the appointment table
+			$addApptPrep = $db->prepare("INSERT INTO Appointment(FK_DoctorID, FK_PatientID, Date, Time, Address, Status, Reason) 
+                                        VALUES(:doc, :pat, :date, :time, :address, :status, :reason);");
+			//$tableType = '';
+			
+			$vals = array(  
+					':doc'=>$doc,         
+					':pat'=>$pat,
+					':date'=>$date,
+					':time'=>$time,
+					':address'=>$address,
+					':status'=>$status,
+					':reason'=>$reason
+			);
+			$insertApptSuccess = $addApptPrep->execute($vals);
+			
+			if(   !$insertApptSuccess  ){
+					$errMsgArr[] = 'update visit failed';
+					$errNum += 1;                           
+			}
+			
+			$retVal = outputXML($errNum, $errMsgArr, $db);
+                        
+        } else {
+            $retVal = outputXML($errNum, $errMsgArr, $db);
+        }
+        
+                        
+        return $retVal; 
+}
+//Type 2 Service
 function doServiceVi($db) {
-
         $errMsgArr = array();
         $errNum = 0;
 		$id = $_POST['id'];
@@ -72,8 +143,9 @@ function doServiceVi($db) {
         $dos = $_POST['dos'];
         $sdate = $_POST['sdate'];
         $edate = $_POST['edate'];
+		$bill = $_POST['bill'];
         $pp = $_POST['pp'];
-		$numon = $_POST['nummonths'];
+		$numon = $_POST['numan'];
 		$rd = $_POST['rd'];
 		$fname = $_POST['fname'];
 		$floc = $_POST['floc'];
@@ -97,91 +169,55 @@ function doServiceVi($db) {
                 $errNum += 1;
         }
         //end
-        
-        
-        //$prepUsers = $db->prepare("SELECT * FROM `Users` WHERE UserName = :id ; ");     
-		/*
-        if ( $prepUsers->execute( array( ":id" => $user ) ) ) { 
-                //IF NAME IS NOT IN USE
-                if($prepUsers->rowCount() != 0){
-                        $errMsgArr[] = 'Username already in use';
-                        $errNum += 1;
-                } 
-        } else {
-                $error = $prepUsers->errorInfo();
-                $errMsgArr[] = $error[2];
-                $errNum += 1;
-                $retVal = outputXML($errNum, $errMsgArr, $db);          
-        }
-        */
-        
-        
-        
-        
         if($errNum == 0){
-                        //set up and insert values into the user table
-                        $updateVistPrep = $db->prepare("INSERT INTO Users(FirstName, LastName, UserName, Email, Birthday, SSN, Type, NeedApproval, Password) 
-                                        VALUES(:fname, :lname, :login, :email, :bday, :ssn, :type, :needapproval, :password);");
-                        //$tableType = '';
-                        
-                        //$needapproval;
-                        //$type;
+			//Do update
+			$updateVistPrep = $db->prepare("UPDATE Appointment SET bp = :bp, weight = :weight, symptoms = :sym, diagnosis = :diag,
+												bill = :bill, paymentPlan = :pp, NumMonths = :numan, FK_ReferalDoc = :rd,
+												fileName = :fname, fileLocation = :floc
+												WHERE PK_AppID = :id");
+			//$tableType = '';
+			
+			$vals = array(  
+					':bp'=>$bp,         
+					':weight'=>$weight,
+					':sym'=>$sym,
+					':diag'=>$diag,
+					':bill'=>$bill,
+					':pp'=>$pp,
+					':numan'=>$numan,
+					':rd'=>$ssn, 
+					':fname'=>$ssn,
+					':floc'=>$floc,
+					':id'=>$id
+			);
+			$updateVisitSuccess = $updateVistPrep->execute($vals);
+			
+			if(   !$insertUserSuccess  ){
+					$errMsgArr[] = 'update visit failed';
+					$errNum += 1;                           
+			}
 
-                        if (strcmp($_POST['type'], "patient") == 0){                            
-                                $type = 1;
-                                $needapproval = 0;
-                                $tableType = "Patient";                                         
-                        } elseif (strcmp($_POST['type'], "nurse") == 0){
-                                $type = 200;
-                                $needapproval = 1;
-                                $tableType = "Nurse";
-                        } elseif (strcmp($_POST['type'], "doctor") == 0){
-                                $type = 300;
-                                $needapproval = 1;
-                                $tableType = "Doctor";
-                        } elseif (strcmp($_POST['type'], "admin") == 0){
-                                $type = 400;
-                                $needapproval = 1;
-                                $tableType = "Admin";
-                        }                       
-                        
-                        $vals = array(  ':type'=>$type,
-                                ':needapproval'=>$needapproval,         
-                                ':fname'=>$fname,
-                                ':lname'=>$lname,
-                                ':login'=>$user,
-                                ':email'=>$email,
-                                ':bday'=>$bday,
-                                ':ssn'=>$ssn,   
-                                ':password'=>md5($password)
-                        );
-                        $insertUserSuccess = $insertUserPrep->execute($vals);
-                        
-                        if(   !$insertUserSuccess  ){
-                                $errMsgArr[] = 'Insert into user table failed';
-                                $errNum += 1;                           
-                        }                       
-                        else {
-                        
-                                //get the primary key for the recently entered row
-                                $memIDPrep = $db->prepare("SELECT * FROM Users WHERE UserName = '" . $user . "'");
-                                $getIDSuccess = $memIDPrep->execute();
-                                if( ! $getIDSuccess ){
-                                        $errMsgArr[] = 'Get user table ID failed';
-                                        $errNum += 1;
-                                } else {
-                                        
-                                        //add into the proper sub table with the user primary key as the member foreign key
-                                        $member = $memIDPrep->fetch(PDO::FETCH_ASSOC);
-                                        $insertTypePrep = $db->prepare("INSERT INTO " .$tableType. "(FK_member_id) VALUES('" .$member['PK_member_id']. "')");
-                                        if( !($insertTypePrep->execute()) ){
-                                                $errMsgArr[] =  "Insert into $tableType table failed";
-                                                $errNum += 1;
-                                        }
-                                }
-                        }
-                        
-                        $retVal = outputXML($errNum, $errMsgArr, $db);
+			if($med && $dos && $sdate && edate){
+				//get the patient id from visit key for the recently entered row
+				$patIDPrep = $db->prepare("SELECT * FROM Appointment WHERE PK_APPTID = '" . $id . "'");
+				//$getIDSuccess = $memIDPrep->execute();
+				$patient = $memIDPrep->fetch(PDO::FETCH_ASSOC);
+				$insertMedPrep = $db->prepare("INSERT INTO Medications (FK_PatientID, Medication, Dosage, StartDate, EndDate)
+												VALUSE( '".$patient['FK_PatientID']."', :med, :dos, :sdate, :edate)");
+				$vals2 = array(  
+					':med'=>$med,         
+					':dos'=>$dos,
+					':sdate'=>$sdate,
+					':edate'=>$edate
+				);
+				if( !($insertMedPrep->execute($vals2)) ){
+						$errMsgArr[] =  "Medication insert fail";
+						$errNum += 1;
+				}
+			}
+		
+			
+			$retVal = outputXML($errNum, $errMsgArr, $db);
                         
         } else {
                 $retVal = outputXML($errNum, $errMsgArr, $db);
@@ -191,27 +227,21 @@ function doServiceVi($db) {
         return $retVal; 
         
 }
-
-function doServiceAp($db) {
-
+//Type 3 service
+function doServiceUp($db) {
+		//NOTE: GET WITH MU TO SEE WHAT HE USING TO POST
         $errMsgArr = array();
         $errNum = 0;
-
-        $bp = $_POST['bp'];
-        $weight = $_POST['weight'];
-        $sym = $_POST['sym'];
-        $diag = $_POST['diag'];
-        $med = $_POST['med'];
-        $dos = $_POST['dos'];
-        $sdate = $_POST['sdate'];
-        $edate = $_POST['edate'];
-        $pp = $_POST['pp'];
-		$numon = $_POST['nummonths'];
-		$rd = $_POST['rd'];
-		$fname = $_POST['fname'];
-		$floc = $_POST['floc'];
-        
-        //Input Validations
+		$id = $_POST[''];
+		$doc = $_POST[''];
+		$pat = $_POST[''];
+        $date = $_POST[''];
+        $time = $_POST[''];
+        $address = $_POST[''];
+        $status = $_POST[''];
+		$reason = $_POST[''];
+        /*
+        //Input Validations (still need to do
         if (!isset($_POST['bp']) || $_POST['bp'] == '') {
                 $errMsgArr[] = 'Blood Pressure missing';
                 $errNum += 1;
@@ -228,103 +258,39 @@ function doServiceAp($db) {
         if (!isset($_POST['diag']) || $_POST['diag'] == '') {
                 $errMsgArr[] = 'Diagnosis address missing';
                 $errNum += 1;
-        }
-        //end
-        
-        
-        //$prepUsers = $db->prepare("SELECT * FROM `Users` WHERE UserName = :id ; ");     
-		/*
-        if ( $prepUsers->execute( array( ":id" => $user ) ) ) { 
-                //IF NAME IS NOT IN USE
-                if($prepUsers->rowCount() != 0){
-                        $errMsgArr[] = 'Username already in use';
-                        $errNum += 1;
-                } 
-        } else {
-                $error = $prepUsers->errorInfo();
-                $errMsgArr[] = $error[2];
-                $errNum += 1;
-                $retVal = outputXML($errNum, $errMsgArr, $db);          
-        }
-        */
-        
-        
-        
+        }*/
         
         if($errNum == 0){
-                        //set up and insert values into the user table
-                        $updateVistPrep = $db->prepare("INSERT INTO Users(FirstName, LastName, UserName, Email, Birthday, SSN, Type, NeedApproval, Password) 
-                                        VALUES(:fname, :lname, :login, :email, :bday, :ssn, :type, :needapproval, :password);");
-                        //$tableType = '';
-                        
-                        //$needapproval;
-                        //$type;
-
-                        if (strcmp($_POST['type'], "patient") == 0){                            
-                                $type = 1;
-                                $needapproval = 0;
-                                $tableType = "Patient";                                         
-                        } elseif (strcmp($_POST['type'], "nurse") == 0){
-                                $type = 200;
-                                $needapproval = 1;
-                                $tableType = "Nurse";
-                        } elseif (strcmp($_POST['type'], "doctor") == 0){
-                                $type = 300;
-                                $needapproval = 1;
-                                $tableType = "Doctor";
-                        } elseif (strcmp($_POST['type'], "admin") == 0){
-                                $type = 400;
-                                $needapproval = 1;
-                                $tableType = "Admin";
-                        }                       
-                        
-                        $vals = array(  ':type'=>$type,
-                                ':needapproval'=>$needapproval,         
-                                ':fname'=>$fname,
-                                ':lname'=>$lname,
-                                ':login'=>$user,
-                                ':email'=>$email,
-                                ':bday'=>$bday,
-                                ':ssn'=>$ssn,   
-                                ':password'=>md5($password)
-                        );
-                        $insertUserSuccess = $insertUserPrep->execute($vals);
-                        
-                        if(   !$insertUserSuccess  ){
-                                $errMsgArr[] = 'Insert into user table failed';
-                                $errNum += 1;                           
-                        }                       
-                        else {
-                        
-                                //get the primary key for the recently entered row
-                                $memIDPrep = $db->prepare("SELECT * FROM Users WHERE UserName = '" . $user . "'");
-                                $getIDSuccess = $memIDPrep->execute();
-                                if( ! $getIDSuccess ){
-                                        $errMsgArr[] = 'Get user table ID failed';
-                                        $errNum += 1;
-                                } else {
-                                        
-                                        //add into the proper sub table with the user primary key as the member foreign key
-                                        $member = $memIDPrep->fetch(PDO::FETCH_ASSOC);
-                                        $insertTypePrep = $db->prepare("INSERT INTO " .$tableType. "(FK_member_id) VALUES('" .$member['PK_member_id']. "')");
-                                        if( !($insertTypePrep->execute()) ){
-                                                $errMsgArr[] =  "Insert into $tableType table failed";
-                                                $errNum += 1;
-                                        }
-                                }
-                        }
-                        
-                        $retVal = outputXML($errNum, $errMsgArr, $db);
+			//set up and insert values into the appointment table
+			$upApptPrep = $db->prepare("UPDATE Appointment SET FK_DoctorID = :doc, FK_PatientID = :pat, Date = :date, Time = :time, 
+											Address = :address, Status = :status, Reason = :reason WHERE PK_AppID = :id");
+			//$tableType = '';
+			$vals = array(  
+					':doc'=>$doc,         
+					':pat'=>$pat,
+					':date'=>$date,
+					':time'=>$time,
+					':address'=>$address,
+					':status'=>$status,
+					':reason'=>$reason,
+					':id' => $id
+			);
+			$insertApptSuccess = $upApptPrep->execute($vals);
+			
+			if(   !$insertApptSuccess  ){
+					$errMsgArr[] = 'update visit failed';
+					$errNum += 1;                           
+			}
+			
+			$retVal = outputXML($errNum, $errMsgArr, $db);
                         
         } else {
-                $retVal = outputXML($errNum, $errMsgArr, $db);
+            $retVal = outputXML($errNum, $errMsgArr, $db);
         }
         
                         
         return $retVal; 
-        
 }
-
 
 
 
