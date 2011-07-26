c039f985285319e578b2d12bbef6192924671cc1
and1balla2424 <AND1Balla2424@gmail.com>
2011-07-26 01:44:36 +0000

    trying to get info on patient.

--- emis-cs/Electronic-MIS/Electronic MIS/PatientInfoTab.cs
+++ emis-cs/Electronic-MIS/Electronic MIS/PatientInfoTab.cs
@@ -6,17 +6,26 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Net;
+using System.Security;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;   
 
 namespace Electronic_MIS
 {
     public partial class PatientInfoTab : UserControl
     {
+
+
         SessionManager sessionManager;
+        List<Patient> patients;
 
         public PatientInfoTab(SessionManager manager)
         {
             InitializeComponent();
             this.sessionManager = manager;
+            patients = new List<Patient>();
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -28,5 +37,98 @@ namespace Electronic_MIS
         {
 
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PatientInfoTab_Load(object sender, EventArgs e)
+        {
+            //Build the Connection String
+            UriBuilder ub = new UriBuilder();
+
+            StringBuilder data = new StringBuilder();
+            data.Append("u=" + WebUtility.HtmlEncode(sessionManager.User));
+            data.Append("&key=" + WebUtility.HtmlEncode(sessionManager.Key));
+
+            ub.Host = "localhost/emis/emis-dev3/viewPatientREST.php";
+            ub.Query = data.ToString();
+
+            
+            //Create the request
+            Uri requestUri = ub.Uri;
+            WebRequest request = WebRequest.Create(requestUri);
+            request.Method = "GET";
+            
+            
+            try
+            {
+                WebResponse response = request.GetResponse();
+                //StreamReader reader = new StreamReader(response.GetResponseStream);
+                //Debug.WriteLine(reader.ReadToEnd());
+
+                XmlTextReader xmlReader = new XmlTextReader(response.GetResponseStream());
+                
+                //testing to put input inside textbox1, first name textbox
+                //textBox1.Text = "juan";
+                
+                while (xmlReader.Read())
+                {
+                    switch (xmlReader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            if (xmlReader.Name == "PatientCount")
+                            {
+                                Patient newpat = new Patient();
+                                xmlReader.Read();
+                                while (xmlReader.Name != "patientinfo")
+                                {
+                                    xmlReader.Read();
+                                    switch (xmlReader.Name)
+                                    {
+                                        case "FirstName":
+                                            newpat.FirstName = xmlReader.ReadElementContentAsString();
+                                            break;
+
+                                    }
+                                }
+
+
+                            }
+                            break;
+                            default:
+                            break;
+                    }
+                }
+                
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message, "Yeah...we didn't plan for this", MessageBoxButtons.OK);
+                Application.Exit();
+            }
+            
+            
+        }
     }
+
+    class Patient
+    {
+        String firstn;
+
+        public String FirstName
+        {
+            get
+            {
+                return firstn;
+            }
+            set
+            {
+                firstn = value;
+            }
+
+        }
+    }
+
 }
