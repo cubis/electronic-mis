bdba5c76efbbe73cd8e4610712f8a6db1777d0d6
lgadban <lgadban@942dca17-fbde-0292-d59d-a099d8e55517>
2011-07-26 02:45:46 +0000

    Fixed link

--- emis-dev3/generateNav.php
+++ emis-dev3/generateNav.php
@@ -5,7 +5,7 @@
     </div>
 <?php elseif ($_SESSION['SESS_TYPE'] >= 400): // Logged in user is an Admin ?>
 	<div class="nav_title">Administrator</div>
-	<div class="navlist"><a href="admin/editMembersView.php">View/Edit Members</a></div>
+	<div class="navlist"><a href="admin/editMemberListView.php">View/Edit Members</a></div>
 	<div class="navlist"><a href="admin/approveRequestsView.php">View/Approve/Deny Member Request</a></div>
 <?php elseif ($_SESSION['SESS_TYPE'] == 300): // Logged in user is a doctor ?>
 	<div class="nav_title">Doctor</div>
