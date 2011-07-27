4a48e7bae0ea70b9489811892f9f5bfa67861d4b
basilsattler <basilsattler@gmail.com>
2011-07-27 16:55:53 +0000

    notify works bitches.. fuck you all

--- emis-dev3/notify.php
+++ emis-dev3/notify.php
@@ -2,81 +2,53 @@
 require_once('configREST.php');    
 require_once('bootstrapREST.php');
 
-/*
-plan:
-    - authenticate requestor
-    - determin requestType - {notify, logout}
-    - notify
-        - get list of users where appointment < 24 hours and wants notify=true
-            -required info {apointmentDate, Time, Doctor}
-        - send notification e-mail, on success set notify=false
-            -succesful notifaction makes action log entry
-            
-            
-            
-            
-            
-            
-            
-SELECT Appointment.PK_AppID, Appointment.Date, Appointment.Time, Appointment.Reminder, Users.Email, Users.FirstName, Users.LastName
-FROM Appointment, Patient, Users
-WHERE
-    Appointment.FK_PatientID = Patient.PK_PatientID
-AND Patient.FK_member_id = Users.PK_member_id
-AND Appointment.Date+0 <= CURDATE() + 1
-AND Appointment.Reminder = 1;
+// I had to add this, quick fix my connection string is not working... please refactor
+ $connection = @mysql_connect("devdb.fulgentcorp.com","495311team2user","680c12D5!gP592xViF") or die(mysql_error());
+                $database = @mysql_select_db("cs49532011team2", $connection) or die(mysql_error());
 
-
-
-*/
-
-    echo "<p>starting</p>";
-    echo "<p>method = ".$_GET['mail']."</p>";
-    
-    $qry = "
-SELECT Appointment.PK_AppID, Appointment.Date, Appointment.Time, Appointment.Reminder, Users.Email, Users.FirstName, Users.LastName
-FROM Appointment, Patient, Users
-WHERE Appointment.FK_PatientID = Patient.PK_PatientID
-AND Patient.FK_member_id = Users.PK_member_id
-AND Appointment.Date+0 <= CURDATE() + 1
-AND Appointment.Reminder = 1";
+function sendReminders(){
+    $qry = "SELECT Appointment.PK_AppID, Appointment.Date, Appointment.Time, Appointment.Reminder, Users.Email, Users.FirstName, Users.LastName
+            FROM Appointment, Patient, Users
+            WHERE Appointment.FK_PatientID = Patient.PK_PatientID
+            AND Patient.FK_member_id = Users.PK_member_id
+            AND Appointment.Date+0 <= CURDATE() + 1
+            AND Appointment.Reminder = 1";
 
     $result=mysql_query($qry);
-    foreach($row as mysql_fetch_array($result)){
-        echo $row;
-    }
-     
 
+// main dispatching loop
+    while ($row = mysql_fetch_array($result)){
+        if(sendMail($row['Email'],$row['Date'],$row['Time'])){
+            $qry = "UPDATE Appointment SET Reminder='0' WHERE PK_AppID=".$row['PK_AppID'];
+        }
+    }
+ }
+
+  // task manager
     switch( $_GET['method']){
         case "talk":
             print "<p> Hello! </p>\n";
             break;
         
-        case "sendReminders":
-                       
+        case "notify":
+            sendReminders();
             break;
          
         default:
             print "<p> this is a generic response</p>";   
-    } 
-    
-
+    }
 
     function sendMail($address, $date, $time){
-
-        echo "<p>user is: ".$_GET['u']."</p>";
-        
         $to = $address;
-        $subject = "Test mail from emis";
-        $message = "this should work, Hello! This is a simple email message.\n no action required!";
+        $subject = "Apointment Reminder";
+        $message = "Hello, This is a reminder that you have an appointment on $date at $time";
         $from = "cpe-67-10-181-224.satx.res.rr.com";
         $headers = "From:" . $from;
 
-
         if( mail($to,$subject,$message,$headers)){
-            echo "<p>Mail Sent. to $to</p>";
+            return true; //mail was sent
         }else
-            echo "<p>error occured in mail() function for $to</p>";
+            return false; //mail not sent error with server
         }
 
 ?>
