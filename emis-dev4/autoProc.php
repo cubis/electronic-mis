630a08fe6a6a3893c4f32d466b7d35b3efe85888
basilsattler <basilsattler@gmail.com>
2011-08-14 01:54:56 +0000

    authenticates!! :)

--- emis-dev4/autoProc.php
+++ emis-dev4/autoProc.php
@@ -61,12 +61,30 @@
   ////////////////////////////////////// insert autoproc code here ///////////////////////////////////////////
 
 
-require_once('configREST.php');
-require_once('bootstrapREST.php');
+print("<p>autoproc start</p>");
+//require_once('configREST.php');
+//require_once('bootstrapREST.php');
+
+print("<p>autoproc after header</p>");
 
 // I had to add this, quick fix my connection string is not working... please refactor
- $connection = @mysql_connect("devdb.fulgentcorp.com","495311team2user","680c12D5!gP592xViF") or die(mysql_error());
+$connection = @mysql_connect("devdb.fulgentcorp.com","495311team2user","680c12D5!gP592xViF") or die(mysql_error());
                 $database = @mysql_select_db("cs49532011team2", $connection) or die(mysql_error());
+
+
+    function sendMail($address, $date, $time){
+        $to = $address;
+        $subject = "Apointment Reminder";
+        $message = "Hello, This is a reminder that you have an appointment on $date at $time";
+        $from = "cpe-67-10-181-224.satx.res.rr.com";
+        $headers = "From:" . $from;
+
+       if( mail($to,$subject,$message,$headers)){
+	    print("<p>mail was sent to: $to</p>\n");
+            return true; //mail was sent
+        }else
+            return false; //mail not sent error with server
+     }
 
 function sendReminders(){
     $qry = "SELECT Appointment.PK_AppID, Appointment.Date, Appointment.Time, Appointment.Reminder, Users.Email, Users.FirstName, Users.LastName
@@ -75,14 +93,13 @@ function sendReminders(){
             AND Patient.FK_member_id = Users.PK_member_id
             AND Appointment.Date+0 <= CURDATE() + 1
             AND Appointment.Reminder = 1";
-
     $result=mysql_query($qry);
-
 // main dispatching loop
     while ($row = mysql_fetch_array($result)){
+	print("<p>email=".$row['Email']." date:".$row['Date']."</p>");
         if(sendMail($row['Email'],$row['Date'],$row['Time'])){
-            $qry = "UPDATE Appointment SET Reminder='0' WHERE PK_AppID=".$row['PK_AppID'];
-            mysql_query($qry);
+            //$qry = "UPDATE Appointment SET Reminder='0' WHERE PK_AppID=".$row['PK_AppID'];
+           // mysql_query($qry);
         }
     }
  }
@@ -96,36 +113,19 @@ function sendReminders(){
         case "notify":
             sendReminders();
             break;
+
         case "reset":
              $rstqry = "Update Appointment set Reminder = '1' ";
-             $mysql_query($qry);
+             $mysql_query($rsqry);
              echo "<p>Notifications Reset</p>";
             break;
     }
 
-    function sendMail($address, $date, $time){
-        $to = $address;
-        $subject = "Apointment Reminder";
-        $message = "Hello, This is a reminder that you have an appointment on $date at $time";
-        $from = "cpe-67-10-181-224.satx.res.rr.com";
-        $headers = "From:" . $from;
-
-        if( mail($to,$subject,$message,$headers)){
-            return true; //mail was sent
-        }else
-            return false; //mail not sent error with server
-        }
 
 
-
-
-
-
-
-
+	}
 
 /////////////////////////////////////////////end of autoproc code/////////////////////////////////////////////
-		}
 		exit();
 		//Login failed
 
