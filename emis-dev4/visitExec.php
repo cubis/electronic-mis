a10c36896a4a59e2c97697e2f26302205287bab9
lgadban <lgadban@942dca17-fbde-0292-d59d-a099d8e55517>
2011-08-15 09:10:08 +0000

    stuff

--- emis-dev4/visitExec.php
+++ emis-dev4/visitExec.php
@@ -5,94 +5,90 @@ require_once('auth.php');
 require_once('bootstrap.php');
 
 		
-		$errmsg_arr = array();
-		global $currentPath;
-		$url = $currentPath . "visitREST.php";
-		//formating stuff for array;
-		$fname;
-		$fname = $_FILES['uploadfile']['name'];
-		$tmpname = $_FILES['uploadfile']['tmp_name'];
-		$fsize = $_FILES['uploadfile']['size'];
-		$ftype = $_FILES['uploadfile']['type'];
-		//$fname = addslashes($fname);
-		//$apNum = $_GET['ID'];
-		//test the type
-		$fp = fopen($tmpname,'rb');
-		$content = fread($fp,filesize($tmpname));
-		$content = addslashes($content);
-		//$fclose($fp);
+	$errmsg_arr = array();
+	$uploaddir = 'C:\\xampp\\htdocs\\emis\\upload\\';
+	$uploadfile = $uploaddir . basename($_FILES['uploadfile']['name']);
+	$filesize = $_FILES['uploadfile']['size'];
 
-		if(!get_magic_quotes_gpc()){
-			$fname = addslashes($fname);
-		}
-		//put stuff in array
-		$fields = array(
-			'u' => urlencode($_SESSION['SESS_USERNAME']),
-			'key' => urlencode($_SESSION['SESS_AUTH_KEY']),
-			'aid' => urlencode($_POST['aid']),
-			'bp' => urlencode($_POST['BP']),
-			'weight' => urlencode($_POST['Weight']),
-			'symptoms' => urlencode($_POST['Symptoms']),
-			'diagnosis' => urlencode($_POST['Diagnosis']),
-			'medicine' => urlencode($_POST['Medicine']),
-			'dosage' => urlencode($_POST['Dosage']),
-			'startDate' => urlencode($_POST['StartDate']),
-			'endDate' => urlencode($_POST['EndDate']),
-			'totalBill' => urlencode($_POST['Bill']),
-			'fname' => urlencode($fname),
-			'fsize' => urlencode($fsize),
-			'ftype' => urlencode($ftype),
-			'content' => urlencode($content)
-		);
-		
-		if(isset($_POST['aid'])){
-			$fields['aid'] = urlencode($_POST['aid']);
-		}
-    
-		foreach($fields as $key=>$value){
-			$field_string .= $key.'='.$value.'&';
-		}
-		$field_string = rtrim($field_string, '&');
-		
-		//die($field_string);
-		
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_POST, sizeof($fields));
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $field_string);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 4);    
-		curl_setopt($ch, CURLOPT_TIMEOUT, 8);
-		curl_setopt($ch, CURLOPT_HEADER, false);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		$output = curl_exec($ch);
-		curl_close($ch);
-   
-		$parser = xml_parser_create();
-		
-		xml_parse_into_struct($parser, $output, $wsResponse, $wsIndices);
-		
+	//move_uploaded_file($_FILES['uploadfile']['tmp_name'], $uploadfile);
+	
+	/*echo '<pre>';
+	*/
+	if (move_uploaded_file($_FILES['uploadfile']['tmp_name'], $uploadfile)) {
+		//echo "File is valid, and was successfully uploaded.\n";
+	} else {
+		die("Possible file upload attack!\n");
+	}
+	
+	$fields = array(
+		'u' => urlencode($_SESSION['SESS_USERNAME']),
+		'key' => urlencode($_SESSION['SESS_AUTH_KEY']),
+		'aid' => urlencode($_POST['aid']),
+		'bp' => urlencode($_POST['BP']),
+		'weight' => urlencode($_POST['Weight']),
+		'symptoms' => urlencode($_POST['Symptoms']),
+		'diagnosis' => urlencode($_POST['Diagnosis']),
+		'reason' => urlencode($_POST['Reason']),
+		'medicine' => urlencode($_POST['Medicine']),
+		'dosage' => urlencode($_POST['Dosage']),
+		'startDate' => urlencode($_POST['StartDate']),
+		'endDate' => urlencode($_POST['EndDate']),
+		'totalBill' => urlencode($_POST['Bill']),
+		'fileName' => urlencode($uploadfile),
+		'fileSize' => urlencode($filesize)
+	);
+
+	if(isset($_POST['aid'])){
+		$fields['aid'] = urlencode($_POST['aid']);
+	}
+
+	foreach($fields as $key=>$value){
+		$field_string .= $key.'='.$value.'&';
+	}
+	$field_string = rtrim($field_string, '&');
+
+	//die($field_string);
+
+	global $currentPath;
+	$url = $currentPath . "visitREST.php";
+	//die($url);
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_POST, sizeof($fields));
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $field_string);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 4);    
+	curl_setopt($ch, CURLOPT_TIMEOUT, 8);
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$output = curl_exec($ch);
+	curl_close($ch);
+
+	$parser = xml_parser_create();
+
+	xml_parse_into_struct($parser, $output, $wsResponse, $wsIndices);
+
 	//	print("output = " . $output);
 	//	print_r($_POST);
 	die($output);
 
-		$errNum = $wsResponse[$wsIndices['ERRNUM'][0]]['value'];
-		
-		
-		if($errNum == 0){
-			$errmsg_arr[] = "Appointment Updated Successfully";
-			$_SESSION['ERRMSG_ARR'] = $errmsg_arr;
-			$thisAid = $wsResponse[$wsIndices['APPTID'][0]]['value'];
-			header("location: apptEditView.php?aid=$thisAid"); 
-		} else {
-			$ct = 0;
-			while($ct < $errNum){
-				$errmsg_arr[] = $wsResponse[$wsIndices['ERROR'][$ct]]['value'];
-				$ct++;
-			}
-			$_SESSION['ERRMSG_ARR'] = $errmsg_arr;
-			header("location: apptEditView.php");
+	$errNum = $wsResponse[$wsIndices['ERRNUM'][0]]['value'];
+
+
+	if($errNum == 0){
+		$errmsg_arr[] = "Appointment Updated Successfully";
+		$_SESSION['ERRMSG_ARR'] = $errmsg_arr;
+		$thisAid = $wsResponse[$wsIndices['APPTID'][0]]['value'];
+		header("location: apptEditView.php?aid=$thisAid"); 
+	} else {
+		$ct = 0;
+		while($ct < $errNum){
+			$errmsg_arr[] = $wsResponse[$wsIndices['ERROR'][$ct]]['value'];
+			$ct++;
 		}
-		
-		session_write_close();
-		
+		$_SESSION['ERRMSG_ARR'] = $errmsg_arr;
+		header("location: apptEditView.php");
+	}
+
+	session_write_close();
+	
 ?>
  
--- emis-dev4/visitREST.php
+++ emis-dev4/visitREST.php
@@ -1,19 +1,14 @@
 <?php
 require_once('configREST.php');     //sql connection information
 require_once('bootstrapREST.php');  //link information
+require_once('bootstrap.php');  //link information
 
 clean(&$_POST);
 $output = doService();
 print($output);
 
 function outputXML($errNum, $errMsgArr, $memberInfo) {
-/* @var $AUTH_KEY A key that will be used to prove authentication occurred from this service. */
-	global $db;
-	if( isset($_POST['u']) ){
-		$user = $_POST['u'];
-	} else {
-		$user = "UNKOWN";
-	}
+
 	$outputString = ''; //start empty
 	$outputString .= "<?xml version=\"1.0\"?>\n";
 	$outputString .= "<content><errNum>" . $errNum . "</errNum>\n";
@@ -26,16 +21,15 @@ function outputXML($errNum, $errMsgArr, $memberInfo) {
 			$outputString .= "<ERROR>" . $errMsgArr[$ct] . "</ERROR>\n";
 			$ct++;
 		}
-		if( !isset($memberInfo['PK_member_id']) ){
+		/*if( !isset($memberInfo['PK_member_id']) ){
 			logToDB($user." failed to change password", NULL, $user);
 		} else {		
 			logToDB($user." failed to change password", $memberInfo['PK_member_id'], $user);
-		}		
+		}*/
 	}
 		
 	$outputString .= "</content>";
 	return $outputString;
-	//return "SHIT";
 }
 
 function doService() {
@@ -44,7 +38,6 @@ function doService() {
 		
 		$errMsgArr = array();
 		$errNum = 0;
-		
 		
 		//MAKE SURE THEY PASSED US CREDENTIALS	
 		if(!isset($_POST['u']) || $_POST['u'] == ''){
@@ -77,105 +70,99 @@ function doService() {
 		$trustString = "xolJXj25jlk56LJkk5677LS";
 		$trustedKey = md5($currKey.$trustString);
 		
-		if($recKey == $trustedKey){
-		
-				$updateSQL = 'INSERT INTO Visit 
-				(Visit.FK_AppID, Visit.BP, Visit.Weight, 
-				 Visit.Reason, Visit.Diagnosis, Visit.Symptoms, Visit.Medicine, 
-				 Visit.Dosage, Visit.StartDate, Visit.EndDate, Visit.Bill)';
-				$updateSQL .= ' VALUES (:aid, :BP, :Weight, :Reason, :Diagnosis, :Symptoms, 
-				:Medicine, :Dosage, :StartDate, :EndDate, :Bill)';
+		if($recKey == $trustedKey) {
 				
-				$updateSQL2 = "UPDATE Appointment SET Status='Completed',
-							bp = :BP, weight = :Weight, symptoms = :Symptoms,
-							diagnosis = :Diagnosis, bill = :Bill
-							WHERE PK_AppID = :aid";
-				
-				$paramArray = array(
-					":aid" => $_POST['aid'],
-					":BP" => $_POST['bp'],
-					":Weight" => $_POST['weight'],
-					":Reason" => $_POST['reason'],
-					":Diagnosis" => $_POST['diagnosis'],
-					":Symptoms" => $_POST['symptoms'],
-					":Medicine" => $_POST['medicine'],
-					":Dosage" => $_POST['dosage'],
-					":StartDate" => $_POST['startDate'],
-					":EndDate" => $_POST['endDate'],
-					":Bill" => $_POST['totalBill']
-				);
-				
-				$prep2 = $db->prepare($updateSQL2);
-				$updateSucc = $prep2->execute($paramArray);
-				if (!$updateSucc) {
-					$errorInfoArray = $prep->errorInfo();
-					$errMsgArr[] = $errorInfoArray[2];
-					$errNum++;					
-					return outputXML($errNum, $errMsgArr, $memberInfo);
-				}
-				
-				$prep = $db->prepare($updateSQL);
-				$updateSucc = $prep->execute($paramArray);
-				if (!$updateSucc) {
-					$errorInfoArray = $prep->errorInfo();
-					$errMsgArr[] = $errorInfoArray[2];
-					$errNum++;					
-					return outputXML($errNum, $errMsgArr, $memberInfo);
-				}
-				//upload file
-				$updateSQL3 = "INSERT INTO Files(Name, Size, Type, Content, FK_ApptID)
-								VALUES(:fname,:fsize,:ftype,:content,:aid)";
-				
-				$paramArray2 = array(
-					":fname" => $_POST['fname'],
-					":fsize" => $_POST['fsize'],
-					":ftype" => $_POST['ftype'],
-					":content" => $_POST['content'],
-					":aid" => $_POST['aid']
-				);
-				
-				$prep3 = $db->prepare($updateSQL3);
-				$updateSucc = $prep->execute($paramArray2);
-				if (!$updateSucc) {
-					$errorInfoArray = $prep->errorInfo();
-					$errMsgArr[] = $errorInfoArray[2];
-					$errNum++;					
-					return outputXML($errNum, $errMsgArr, $memberInfo);
-				}
-				
-				$updateSQL4 = "INSERT INTO Medications(FK_PatientID, Medication, Dosage)
-								VALUES(:pid,:Medicine,:Dosage)";
-				
-				$pidq = "SELECT FK_PatientID FROM Appointment WHERE PK_AppID = '".$_POST['aid']."'";
-				$pid = mysql_query($pidq);
+			$updateSQL2 = "UPDATE Appointment SET Status='Completed', Reason=:Reason, 
+						bp = :BP, weight = :Weight, symptoms = :Symptoms,
+						diagnosis = :Diagnosis, bill = :Bill, fileLocation = :file, fileSize = :fsize 
+						WHERE PK_AppID = :aid";
+			
+			$paramArray = array(
+				":aid" => $_POST['aid'],
+				":BP" => $_POST['bp'],
+				":Weight" => $_POST['weight'],
+				":Reason" => $_POST['reason'],
+				":Diagnosis" => $_POST['diagnosis'],
+				":Symptoms" => $_POST['symptoms'],
+				":Bill" => $_POST['totalBill'],
+				":file" => $_POST['fileName'],
+				":fsize" => $_POST['fileSize']
+			);
+			
+			$prep2 = $db->prepare($updateSQL2);
+			$updateSucc = $prep2->execute($paramArray);
+			if (!$updateSucc) {
+				$errorInfoArray = $prep2->errorInfo();
+				//$errMsgArr[] = $errorInfoArray[2];
+				$errMsgArr[] = "prep2";
+				$errNum++;					
+				return outputXML($errNum, $errMsgArr, $memberInfo);
+			}
+			
+			global $currentPath;
+			$request = $currentPath . "apptViewREST.php?";
+			$request .= "u=" . urlencode($user);
+			$request .= "&key=" . urlencode($recKey);
+			$request .= "&aid=" . urlencode($aid);
 
-				$paramArray3 = array(
-					":Medicine" => $_POST['medicine'],
-					":Dosage" => $_POST['dosage'],
-					":pid" => $pid
-				);
-				
-				$prep4 = $db->prepare($updateSQL4);
-				$updateSucc = $prep->execute($paramArray3);
-				if (!$updateSucc) {
-					$errorInfoArray = $prep->errorInfo();
-					$errMsgArr[] = $errorInfoArray[2];
-					$errNum++;					
-					return outputXML($errNum, $errMsgArr, $memberInfo);
+			//die($request);
+			
+			//format and send request
+			$ch = curl_init($request);
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 4);    
+			curl_setopt($ch, CURLOPT_TIMEOUT, 8);
+			curl_setopt($ch, CURLOPT_HEADER, false);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$RESToutput = curl_exec($ch); //send URL Request to RESTServer... returns string
+			curl_close($ch); //string from server has been returned <XML> closethe channel
+
+			//die($RESToutput);
+
+			if( $RESToutput == ''){
+			  die("CONNECTION ERROR");
+			}
+
+
+			//parse return string
+			$parser = xml_parser_create();	
+			xml_parse_into_struct($parser, $RESToutput, $wsResponse, $wsIndices);
+			xml_parser_free($parser);
+
+			$errNum = $wsResponse[$wsIndices['ERRNUM'][0]]['value'];
+			if ($errNum != 0) {
+				$ct = 0;
+				while($ct < $errNum){
+					$err_msg_arr[] = $wsResponse[$wsIndices['ERROR'][$ct]]['value'];
+					$ct++;
 				}
+				$_SESSION['ERRMSG_ARR'] = $err_msg_arr;
+			}
+
+			
+			
+			$pid = $wsResponse[$wsIndices['PATID'][0]]['value'];
+			//die("FK_PATIENTID " . $pid);
+
+			$updateSQL4 = "INSERT INTO Medications(FK_PatientID, Medication, Dosage)
+							VALUES(:pid, :Medicine, :Dosage)";
+			
+			$paramArray3 = array(
+				":Medicine" => $_POST['medicine'],
+				":Dosage" => $_POST['dosage'],
+				":pid" => $pid
+			);
+			
+			$prep4 = $db->prepare($updateSQL4);
+			$updateSucc = $prep4->execute($paramArray3);
+			if (!$updateSucc) {
+				$errorInfoArray = $prep->errorInfo();
+				//$errMsgArr[] = $errorInfoArray[2];
+				$errMsgArr[] = "DATABASE ERROR TWO";
+				$errNum++;					
+				return outputXML($errNum, $errMsgArr, $memberInfo);
+			}
 				
 				
-				$qry = "UPDATE Appointment SET Status='Completed' WHERE PK_AppID = :aid";
-	
-        $patientNamePrep = $db->prepare($qry);
-        $patientNameSuccess = $patientNamePrep->execute(array(':aid' => $aid));
-       if (!$patientNameSuccess) {
-            $errMsgArr[] = "DATABASE ERROR THREE";
-            $errNum++;
-			$arr = $patientNamePrep->errorInfo();
-			die(print_r($arr));
-        }
-        
 				
 			//return $_POST['FK_PatientID'];
 		} else {
@@ -186,29 +173,8 @@ function doService() {
 		
 		//do file stuff
 		
-		$retVal = outputXML($errNum, $errMsgArr, $memberInfo);
-
+	$retVal = outputXML($errNum, $errMsgArr, $memberInfo);
 	return $retVal;
 	
 }
-//8758e4c115ba4669e13a574464488496xolJXj25jlk56LJkk5677LS
-//AUTH KEY 40fc9157068b426ea62b1134d57be6ce
-
-// set up some useful variables
-
-/*
-$updateSQL = 'INSERT INTO Visit ';
-				$updateSQL .= " Visit.FK_AppID=:aid";
-				$updateSQL .= ", Visit.BP=:BP";
-				$updateSQL .= ", Visit.Weight=:Weight";
-				$updateSQL .= ", Visit.Reason=:Reason";
-				$updateSQL .= ", Visit.Diagnosis=:Diagnosis";
-				$updateSQL .= ", Visit.Symptoms=:Symptoms";
-				$updateSQL .= ", Visit.Medicine=:Medicine";
-				$updateSQL .= ", Visit.Dosage=:Dosage";
-				$updateSQL .= ", Visit.StartDate=:StartDate";
-				$updateSQL .= ", Visit.EndDate=:EndDate";
-				$updateSQL .= ", Visit.Bill=:Bill";
-				*/
-
 ?>
