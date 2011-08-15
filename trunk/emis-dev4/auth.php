92932ff97e3da4875adc36c2e89d025450b248fa
cameronjlopez@gmail.com <cameronjlopez@gmail.com@942dca17-fbde-0292-d59d-a099d8e55517>
2011-08-15 02:12:40 +0000

    added restrict access function, updated pdf

--- trunk/emis-dev4/auth.php
+++ trunk/emis-dev4/auth.php
@@ -12,4 +12,17 @@ if ( !isset($_SESSION['SESS_MEMBER_ID']) || trim($_SESSION['SESS_MEMBER_ID']) ==
     header("location: accessDeniedView.php");
     exit();
 }
+
+// Usage: #### (Patient Nurse Doctor Admin)
+// ex: 0001 = Only Admin has Access
+function restrictAccess ($access) {	
+  session_start();
+  $currentAccess = $_SESSION['SESS_TYPE'];
+  if($currentAccess == 1) $i = 0;
+  elseif($currentAccess == 200) $i = 1;
+  elseif($currentAccess == 300) $i = 2;
+  elseif($currentAccess == 400) $i = 3;
+  else header("Location: memberProfileView.php");;
+  if(!intval(substr($access, $i, 1))) header("Location: memberProfileView.php");
+}
 ?>
